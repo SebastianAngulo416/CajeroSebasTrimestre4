318fc891c4fa34251cbd8021bc1fba3608cc1b68
Sebastian Torres <Personajes416@gmail.com>
2024-08-01 21:51:23 -0500

    Commit 2 ingreso

---
+++ Inicio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenido Estimado Usuario! Usted ha entrado al Cajero Automático SEBT");
+        }
+    }
+}
---
+++ Login/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ingreso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenido Estimado Usuario! Usted ha entrado al Cajero Automático SEBT");
+            if (Ingreso())
+            {
+                Console.Beep();
+                Console.WriteLine("Su inicio de sesión o Login fue un exito");
+            }
+            else
+            {
+                Console.Beep();
+                Console.WriteLine("Su Inicio de sesión/Login ha fallado");
+            }
+        }
+        static bool Ingreso()
+        {
+            Console.Write("Ingrese su nombre de usuario: ");
+            string nombreusuario = Console.ReadLine();
+            Console.Write("Ingrese su contraseña: ");
+            string contraseña = Console.ReadLine();
+            return nombreusuario == "Sebastian" && contraseña == "0416"; 
+        }
+    }
+}
